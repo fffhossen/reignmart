253860dc220f8a26eb2ef97fa5e1767089981c61
fffhossen <fffhossen46@gmail.com>
2026-08-05 12:36:51 +0000

    added web procfile and routes

---
+++ reignmart/resources/views/welcome.blade.php
@@ -0,0 +1,12 @@
+<!DOCTYPE html>
+<html>
+<head>
+    <title>ReignMart | Online Shopping</title>
+</head>
+<body>
+    <h1 style="text-align: center; color: #4F46E5; margin-top: 50px;">
+        Welcome to ReignMart Ecommerce Website!
+    </h1>
+    <p style="text-align: center;">Coming Soon...</p>
+</body>
+</html>
